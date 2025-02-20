729a083128092fc4bb554d15821b0ea54dd8dba0
Yogesh Shrestha <iamyogess@gmail.com>
2025-02-20 15:02:56 +0545

    fetch users

--- convex/users.ts
+++ convex/users.ts
@@ -1,5 +1,6 @@
 import { getAuthUserId } from "@convex-dev/auth/server";
 import { query } from "./_generated/server";
+import { checkPermission, VALID_ROLES } from "./lib/permissions";
 
 export const currentUser = query({
   handler: async (ctx) => {
@@ -8,5 +9,17 @@ export const currentUser = query({
       return null;
     }
     return await ctx.db.get(userId);
+  },
+});
+
+export const allUsers = query({
+  handler: async (ctx) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) throw new Error("You are not authenticated!");
+
+    const isAdmin = await checkPermission(ctx, userId, VALID_ROLES.ADMIN);
+    if (!isAdmin)
+      throw new Error("You do not have permission to view all users! ");
+    return await ctx.db.query("users").collect();
   },
 });
--- middleware.ts
+++ middleware.ts
@@ -3,15 +3,15 @@ import {
   createRouteMatcher,
   nextjsMiddlewareRedirect,
 } from "@convex-dev/auth/nextjs/server";
-import { useQuery } from "convex/react";
-import { api } from "./convex/_generated/api";
+// import { useQuery } from "convex/react";
+// import { api } from "./convex/_generated/api";
 
 const isAuthRoute = createRouteMatcher(["/register", "/login"]);
 const isProtectedRoute = createRouteMatcher(["/new-story"]);
 const isAdminRoute = createRouteMatcher(["/admin/dashboard"]);
 
-const me = useQuery(api.auth.getMe);
-const isAdmin = me?.role === "admin";
+// const me = useQuery(api.auth.getMe);
+// const isAdmin = me?.role === "admin";
 
 export default convexAuthNextjsMiddleware(async (req, { convexAuth }) => {
   const isAuthenticated = await convexAuth.isAuthenticated();
@@ -24,9 +24,9 @@ export default convexAuthNextjsMiddleware(async (req, { convexAuth }) => {
     return nextjsMiddlewareRedirect(req, "/register");
   }
 
-  if (isAdminRoute(req) && isAuthenticated && isAdmin) {
-    return nextjsMiddlewareRedirect(req, "/admin/dashboard");
-  }
+  // if (isAdminRoute(req) && isAuthenticated && isAdmin) {
+  //   return nextjsMiddlewareRedirect(req, "/admin/dashboard");
+  // }
 });
 
 export const config = {
